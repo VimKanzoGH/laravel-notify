2688f55addcccf61d23628561c0fb19c69c1ba06
Damilola Olowookere <damms005@gmail.com>
2022-08-07 14:14:36 +0100

    Dependent on user's Tailwind installation
    Taiwind is now required as a dependeny (together with Alpine JS)

--- src/helpers.php
+++ src/helpers.php
@@ -51,13 +51,3 @@ if (! function_exists('notifyJs')) {
         return '<script type="text/javascript" src="'.asset('vendor/mckenziearts/laravel-notify/js/notify.js').'"></script>';
     }
 }
-
-if (! function_exists('notifyCss')) {
-    /**
-     * @return string
-     */
-    function notifyCss(): string
-    {
-        return '<link rel="stylesheet" type="text/css" href="'.asset('vendor/mckenziearts/laravel-notify/css/notify.css').'"/>';
-    }
-}
